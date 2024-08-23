661971a3e46502b6c932e2938549dc6f37e817c6
ljcui1 <yangyangjiaocui@gmail.com>
2024-08-23 12:46:22 -0700

    Demo Platformer movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private float dir = 0f;
+
+    [SerializeField] private LayerMask groundLayers;
+
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpSpeed = 7f;
+
+    bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dir = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dir * moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, groundLayers);
     }
 }
